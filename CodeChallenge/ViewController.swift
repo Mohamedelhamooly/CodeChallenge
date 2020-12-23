f7db2b0220cb4502717423ec5b5bf85bb6aa472c
mohamed elhamoly <melhamoly@codepro.sa>
2020-12-23 17:37:44 +0200

    Initial Commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Mohamed Elhamoly on 12/23/20.
+//  Copyright © 2020 mohamed Elhamoly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
